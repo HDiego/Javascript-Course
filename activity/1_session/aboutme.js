812044e65cab3fb6830be956daa6c71236dc9839
Diego Rocca <diegorocca15@gmail.com>
2022-06-27 10:11:49 -0300

    Finish activity one for module one

---
+++ activity/1_session/aboutme.js
@@ -0,0 +1,10 @@
+let myName = "Diego Rocca";
+let profession = "Ingeniero en Sistemas";
+let job = "Desarrollador";
+
+function showAboutMe() {    
+    console.log(`Hola, me llamo ${myName} y me dedico a ${profession}.
+Estoy cursando este Máster porque me gusta trabajar en ${job}.`);
+}
+
+export { showAboutMe };
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,5 +1,6 @@
 // Ejercicio 1
-
+import {showAboutMe} from "./aboutme.js";
+showAboutMe();
 console.log("Ejercicio 1");
 
 // Ejercicio 2
@@ -7,8 +8,32 @@ console.log("Ejercicio 1");
 // Ejercicio 3
 
 // Ejercicio 4
+function divisibleSeven() {
+    for(let i=1; i<=100; i++) {
+        if(i % 7 == 0) {
+            console.log(i);
+        }
+    }
+}
+divisibleSeven();
 
 // Ejercicio 5
 
 // Ejercicio 6
 
+class Formatter {
+    constructor(){
+        this.prefix = "Hello";
+    };
+
+    append(text) {
+        console.log(`${this.prefix} ${text}`);
+    }
+}
+let formatter = new Formatter();
+formatter.append("World");
+
+Formatter.prototype.inLowerCase = function(text) {
+    console.log(text.toLowerCase());
+}
+formatter.inLowerCase("I'am Diego");
