a0e353673bc2aba0624c5d9323425f6b4a1eb1e0
Diego Rocca <diegorocca15@gmail.com>
2022-07-03 21:48:25 -0300

    Actividad 2 con todos sus ejercicios

--- activity/2_session/index.js
+++ activity/2_session/index.js
@@ -1,14 +1,114 @@
 // Ejercicio 1
+let person = {
+  name: "Diego",
+  lastname: "Rocca",
+  age: 31,
+  profession: "Developer",
+};
+
+function arrayFromObject(theObject) {    
+  return Object.values(theObject);
+}
+
+//console.log(arrayFromObject(person));
 
 // Ejercicio 2
 
+//Puede referirse al objeto global como windows si se encuentra en un navegador
+//Puede referirse al objeto propietario si se encuentra en un metodo
+//En un evento, refiere al elemento que recibe dicho evento
+
+let sayName = {
+  name: "Diego",
+  useThis() {
+      console.log(`My name is ${this.name}`)
+  }
+}
+//sayName.useThis();
+
+
 // Ejercicio 3
+
+class InvertirCadena {
+  
+  constructor(cadenaInvertir) {
+    this.cadenaInvertir = cadenaInvertir;
+  }
+
+  reverseCadena = () => {
+    if(this.cadenaInvertir == "") {
+      throw new Error("Cadena vacia");
+    }
+    console.log(this.cadenaInvertir.split("").reverse().join(""));
+  }
+}
+
+//let invertirCadena = new InvertirCadena("Hola Mundo");
+//invertirCadena.reverseCadena();
+
+//invertirCadena.nuevoMetodo();
 
 // Ejercicio 4
 
+class Login {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+  }
+
+  login = () => {
+    if(this.username == "admin" && this.password == "passwd") {
+      alert("Usuario Logeado");
+    }
+    else {
+      alert("El usuario o la contraseña son incorrectas")
+    }
+  }
+}
+
+let login = new Login("admin", "passwd");
+//login.login();
+
+let loginBad = new Login("juan", "passwd");
+//loginBad.login();
+
 // Ejercicio 5
 
+const loginButton = document.getElementById("loginSuccess");
+loginButton.addEventListener('click', event => {
+  let login = new Login("admin", "passwd");
+  login.login();
+})
+
+const loginButtonError = document.getElementById("loginFailure");
+loginButtonError.addEventListener('click', event => {
+  let login = new Login("juan", "passwd");
+  login.login();
+})
+
 // Ejercicio 6
+
+const loginSuccessAsync = document.getElementById("loginSuccessAsync");
+loginSuccessAsync.addEventListener('click', event => {
+  loginWithAsync("admin", "passwd");
+})
+
+const loginFailureAsync = document.getElementById("loginFailureAsync");
+loginFailureAsync.addEventListener('click', event => {
+  loginWithAsync("juan", "passwd");
+})
+
+let loginWithAsync = async (username, password) => {
+  try
+  {
+    let promiseResponse = await loginWitUsername(username, password);
+    console.log(promiseResponse);
+  }
+  catch(error)
+  {
+    console.log(error);
+  }
+}
 
 let loginWitUsername = (username, password) => {
     return new Promise(function (resolve, rejected) {
