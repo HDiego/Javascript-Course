af26f8b09d388f09b174259a72a13d336c7e1565
Diego Rocca <diegorocca15@gmail.com>
2022-07-12 15:50:18 -0300

    Actividad 3

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -1,4 +1,30 @@
-// Ejercicio 1
+var arrowFunction = function (person) {
+    var ret = [person.name, person.age, person.profession];
+    return ret;
+};
 // Ejercicio 2
-// Ejercicio 3
+var sumOrConcatenate = function (firstArgument, secondArgument) {
+    if (typeof firstArgument === "number" && typeof secondArgument === "number") {
+        return firstArgument + secondArgument;
+    }
+    if (typeof firstArgument === "string" && typeof secondArgument === "string") {
+        return firstArgument + "-" + secondArgument;
+    }
+    return firstArgument + ":" + secondArgument;
+};
+function vehicleFunction(firstArgument) {
+    return firstArgument.pressPedal !== undefined;
+}
+function callFunctions(firstArgument) {
+    firstArgument.turnOnEngine();
+    if (vehicleFunction(firstArgument)) {
+        firstArgument.pressPedal();
+    }
+    else {
+        firstArgument.openThrottle();
+    }
+}
 // Ejercicio 4
+function removeFirstEntry(firstArgument) {
+    return firstArgument.shift() !== undefined ? firstArgument : [];
+}
--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -1,7 +1,56 @@
 // Ejercicio 1
+interface Person {
+    name: string;
+    age: number;
+    profession: string;
+}
+
+let arrowFunction = (person: Person): Array<string | number> => {
+    let ret : (string | number)[] = [person.name, person.age, person.profession];
+    
+    return ret;
+  }
 
 // Ejercicio 2
+let sumOrConcatenate = (firstArgument: any, secondArgument: any) => {
+    if(typeof firstArgument === "number" && typeof secondArgument === "number") {
+        return firstArgument + secondArgument;
+    }
+    if(typeof firstArgument === "string" && typeof secondArgument === "string") {
+        return firstArgument + "-"+ secondArgument;
+    }
+    return firstArgument + ":"+ secondArgument;
+}
 
 // Ejercicio 3
+interface Car {
+    tires: number;
+    turnOnEngine:()=> void;
+    pressPedal:()=> void;
+}
+
+interface Motorcycle {
+    tires: number;
+    turnOnEngine:()=>void;
+    openThrottle:()=>void;
+}
+
+function vehicleFunction(firstArgument: Car | Motorcycle): firstArgument is Car {
+    return (firstArgument as Car).pressPedal !== undefined;
+}
+
+function callFunctions(firstArgument: Car | Motorcycle): void {
+    firstArgument.turnOnEngine();
+    if(vehicleFunction(firstArgument)) {
+        firstArgument.pressPedal();
+    }
+    else{
+        firstArgument.openThrottle();
+    }
+}
 
 // Ejercicio 4
+function removeFirstEntry<T extends string | number>(firstArgument: T[]): T[] {
+    return firstArgument.shift()!== undefined ? firstArgument : [] ;
+}
+
